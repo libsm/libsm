2c8de053263fb57f86cda0eedfd5598383323c40
vang1ong7ang <vang1ong7ang@outlook.com>
2020-04-08 07:25:17 +0000

    log

--- libsm/sm.py
+++ libsm/sm.py
@@ -1,13 +1,25 @@
+import logging
+
+
 class SM:
-    def __init__(self, fi, fn):
-        N = set()
+    def __init__(self, fi, fn, logger=None):
+
+        self.logger = logging.getLogger(name=logger)
+
         self.V = set()
         self.E = set()
+        N = set()
 
         try:
+            self.logger.info('[SM] [INIT] [START]')
+
             sns = fi()
             for v in sns:
                 N.add(v)
+
+            self.logger.info('[SM] [INIT] [DONE]')
+            self.log()
+            self.logger.info('[SM] [LOOP] [START]')
 
             while True:
                 sn = N.pop()
@@ -19,5 +31,15 @@ class SM:
                     if v in self.V:
                         continue
                     N.add(v)
+
+                self.log(sn=sn, sns=sns)
         except KeyError:
+            self.logger.info('[SM] [LOOP] [DONE]')
             return
+
+    def result(self):
+        return self.V, self.E
+
+    @classmethod
+    def log(self, sn=None, sns=None):
+        pass
